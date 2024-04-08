9bb38c757acd51c0894609d5e8dca8bb83e7890c
Nodira <w1169140@westminster.ac.uk>
2024-04-08 15:49:02 +0100

    "the first commit"

---
+++ first.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.title("This is my first streamlit project")
+st.write("Hello Streamlit!")
+st.write("Hello Streamlit!")
+st.write("one more thing")
+st.title("And one more title")
+st.header("This is the header")
+st.latex(r'''a + ar^1''')
+
+st.checkbox('yes')
+st.button('Click')
+st.radio('Pick you gender', ['Male', 'Female'])
+st.selectbox('Pick your gender', ['Male', 'Gender'])
+st.multiselect('choose a planet', ['Jupiter', 'Mars', 'Neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.slider('Pick a number', 0.50)...
