497e2a9e813104b4dc7c25e9da84c6fedda7008b
Nodira <w1169140@westminster.ac.uk>
2024-04-08 22:23:36 +0100

    adding new graph

--- visuals.py
+++ visuals.py
@@ -5,3 +5,10 @@ rand=np.random.normal(1, 2, size=20)
 fig, ax = plt.subplots()
 ax.hist(rand, bins=15)
 st.pyplot(fig)
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+df= pd.DataFrame( np.random.randn(10, 2), columns=['x', 'y'])
+st.line_chart(df)
